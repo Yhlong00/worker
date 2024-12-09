efb0ba8e33872ebe10daa04feda753352e018eb2
Hailong Yang <hailong.yang@runpod.io>
2024-12-09 14:40:48 -0500

    Update rp_handler.py

--- rp_handler.py
+++ rp_handler.py
@@ -4,16 +4,14 @@ import time
 def handler(event):
     input = event['input']
     
-    instruction = input.get('instruction')  
+    prompt = input.get('prompt')  
     seconds = input.get('seconds', 0)  
 
-    print(f"Received instruction: {instruction}")
+    print(f"Received prompt: {prompt}")
     print(f"Sleeping for {seconds} seconds...")
     time.sleep(seconds)  
-    result = instruction.replace(instruction.split()[0], 'created', 1)
-    print(f"Modified instruction: {result}")
     
-    return result 
+    return prompt 
 
 if __name__ == '__main__':
     runpod.serverless.start({'handler': handler})
